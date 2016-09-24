727bef2de31830035886428c22f243492d8c01e9
Yan Liu <yliu@exou.com>
2016-09-24 01:51:22 -0400

    update sep24 4

--- app/scripts/services/preloadTemplate.js
+++ app/scripts/services/preloadTemplate.js
@@ -2,7 +2,11 @@
  * Created by Yan Liu on 2016-09-24.
  */
 imatrollApp.run(function($templateCache, $http) {
-    $http.get('html/tdTemplate.html').then(function(response) {
-        $templateCache.put('tdTemplate', response.data);
+    $http.get('html/playerTemplate.html').then(function(response) {
+        $templateCache.put('playerTemplate', response.data);
+    });
+
+    $http.get('html/banTemplate.html').then(function(response) {
+        $templateCache.put('banTemplate', response.data);
     });
 });
